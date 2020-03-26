7914633e8401ac50278ff9d100bb37fb8656603e
saileklaj <62044533+saileklaj@users.noreply.github.com>
2020-03-26 16:08:46 -0500

    Add files via upload

---
+++ 3entradas.py
@@ -0,0 +1,49 @@
+import numpy as np
+import preceptron as ptr
+
+class operadores_logicos:
+
+	def operador_and():
+		
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(0),(0),(0),(1)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+		input()
+
+	def operador_or():
+		
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(0),(1),(1),(1)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+
+	def operador_nand():
+		
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(1),(1),(1),(0)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+
+	def operador_nor():
+		
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(1),(0),(0),(0)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+
+	def operador_xor():
+		
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(0),(1),(1),(0)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+
+	def operador_xnor():
+
+		entradas=np.array([[0, 0], [0, 1], [1, 0], [1, 1]],"float32")
+		salida=np.array([(1),(0),(0),(1)],"float32")
+		ptr1=ptr.Preceptron(entradas,salida)
+		ptr1.perceptron()
+
+operadores_logicos.operador_and()
---
+++ preceptron.py
@@ -0,0 +1,26 @@
+"""
+plt.plot(gradpesos[:,0],'K')
+plt.plot(gradpesos[:,1],'X')
+plt.plot(gradpesos[:,2],'Y')
+plt.show()"""
+from keras.models import Sequential
+from keras.layers import Dense
+
+class Preceptron:
+	
+	def __init__(self,entradas,salida):
+		self.entradas=entradas
+		self.salida=salida
+		self.model=Sequential()
+
+	def perceptron(self):
+		
+		self.model.add(Dense(16,input_dim=2, activation='relu'))
+		self.model.add(Dense(1,activation='sigmoid'))
+		self.model.compile(loss='mean_squared_error',
+				optimizer='adam',
+				metrics=['binary_accuracy'])
+		self.model.fit(self.entradas,self.salida,epochs=1000)
+		scores= self.model.evaluate(self.entradas,self.salida)
+		print(self.model.metrics_names[1],scores[1]*1000)
+		print(self.model.predict(self.entradas).round())
